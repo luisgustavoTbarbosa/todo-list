0038535614ea6bd8a5d89afd5025c6691497c65d
Luis Gustavo <gustavobarbosag7@gmail.com>
2024-02-05 06:48:41 -0300

    feat: newTask button icon and adjusting style

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,10 +1,14 @@
 import styles from './NewTask.module.css'
+import { PlusCircle } from "@phosphor-icons/react"
 
 export function NewTask() {
   return (
     <form className={styles.newTaskForm}>
       <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button type="submit">Criar</button>
+      <button type="submit">
+        Criar
+        <PlusCircle size={22} />
+      </button>
     </form>
   )
 }
